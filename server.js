08f3dbbaacf01c09a5768ca77994e8b349d90df1
JadenSimon <sijaden@amazon.com>
2021-07-22 03:19:49 -0700

    test

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env['MY_PORT'], function () {
+  console.log(`Example app listening on port ${MY_PORT}!`)
 })
